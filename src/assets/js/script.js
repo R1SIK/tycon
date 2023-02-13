d3ae0aa3b9f0b05a82f5b34c36082c9827380087
r1sik <nikitanie467@gmail.com>
2023-02-13 20:20:45 +0300

    piska

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,15 +1,15 @@
-const image = document.querySelector(".preview__img");
+// const image = document.querySelector(".preview__img");
 
-function rotate(deg = 0) {
-  image.style.transform = "rotate(" + deg + "deg)";
+// function rotate(deg = 0) {
+//   image.style.transform = "rotate(" + deg + "deg)";
 
-  setTimeout(function () {
-    if (deg === -360) {
-      rotate(0);
-    } else {
-      rotate(deg - 1);
-    }
-  }, 20);
-}
+//   setTimeout(function () {
+//     if (deg === -360) {
+//       rotate(0);
+//     } else {
+//       rotate(deg - 1);
+//     }
+//   }, 20);
+// }
 
-rotate(0);
+// rotate(0);
